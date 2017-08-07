fd7b1ff341c1d2a8f1e9bc9b8001bd28835523d7
adam <Incorrectlyspelt@users.noreply.github.com>
2017-08-07 20:11:05 +0000

    stuffs

--- tracker.py
+++ tracker.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3 
 from peewee import *
-import datetime
+#import datetime
+from datetime import timedelta, datetime
 import os
 import psutil
 from models import *
 from time import sleep
+import logging
 db = SqliteDatabase("temp_track.db")
 
 def main():
+    now = datetime.datetime.utcnow()
     with open("/sys/class/thermal/thermal_zone0/temp") as f:  
         value=str(f.read())
     value = list(value)
@@ -19,27 +22,38 @@ def main():
     for i in value:
         t_value+=i
     value = float(t_value)
-#    print("temp is:",value)
-
-    temp = Temperature(temperature=value)
-    temp.save()
- #   for i in Temperature.select():
-#        print("At {} the computer was {}".format(i.time,i.temperature))
+    Temperature(temperature=value,time=now).save()
     #Begin CPU usage
-    cpu=Cpu(usage = psutil.cpu_percent(interval=None))
-    cpu.save()
-  #  for i in Cpu.select():
-   #     print("At {} the computer was at {}".format(i.time,i.usage))
+    Cpu(usage = psutil.cpu_percent(interval=None),time=now).save()
     #Begin RAM 
-    Ram(percent = psutil.virtual_memory()[2], used = psutil.virtual_memory()[3]).save()
-   # for i in Ram.select():
-    #    print("At {} the computer used {}% of RAM,{}bytes s used".format(i.time,i.percent,i.used)) 
+    Ram(percent = psutil.virtual_memory()[2], used = psutil.virtual_memory()[3],time=now).save()
 
 #db.create_tables([Ram,Temperature,Cpu,Self_usage])
+def clean_db():
+    now =datetime.datetime.utcnow()
+    for i in Ram.select():
+        dt_o = datetime.datetime.strptime(str(i.time), "%Y-%m-%d %H:%M:%S.%f") # convert back to a datetime object
+        if now-timedelta(days=3) > dt_o:
+            i.delete_instance()
+
+    for i in Cpu.select():
+        dt_o = datetime.datetime.strptime(str(i.time), "%Y-%m-%d %H:%M:%S.%f") # convert back to a datetime object
+        if now-timedelta(days=3) > dt_o:
+            i.delete_instance()
+    for i in Temperature.select():
+        dt_o = datetime.datetime.strptime(str(i.time), "%Y-%m-%d %H:%M:%S.%f") # convert back to a datetime object
+        if now- timedelta(days=3) > dt_o:
+            i.delete_instance()
+#if __name__ == "__main__":
+
+#logging.basicConfig(filename="tracker.log",level=logging.DEBUG)
+
 
 while True:  
-    main()
-#    sleep(300)
+  #  main() 
+   clean_db()
+  #  sleep(300)
+
 
 
 
